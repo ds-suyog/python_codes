60750f429bf8a81e50e4979c4c9fa1e14a8f69c9
ds-suyog <suyog.sethia@gmail.com>
2019-06-07 01:50:51 +0530

    decorator sample

--- decorators.py
+++ decorators.py
@@ -1,28 +1,27 @@
 
 # defining a decorator 
 def hello_decorator(func): 
-
     # inner1 is a Wrapper function in which the argument is called  
-    # inner function can access the outer local functions like in this case "func" 
-    def inner():
-        print("Hello, this is before function execution") 
+    # inner1 function can access the outer local functions like in this case "func" 
+    def inner1():
+        print("Hello, this is before myfunc execution") 
   
         # calling the actual function now inside the wrapper function. 
         func() 
   
-        print("This is after function execution") 
+        print("This is after myfunc execution") 
           
-    return inner 
+    return inner1
   
   
 # defining a function, to be called inside wrapper 
+@hello_decorator
 def myfunc(): 
     print("This is inside the function !!") 
   
-  
-# passing 'function_to_be_used' inside the 
-# decorator to control its behavior 
-myfunc = hello_decorator(myfunc) 
+# Either do this or write @hello_decorator above definition of 'myfunc'
+# passing 'myfunc' inside the decorator to control its behavior 
+# myfunc = hello_decorator(myfunc) 
   
   
 # calling the function 
