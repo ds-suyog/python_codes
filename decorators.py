2731bfac79b701a35f40ea196efcc48bf7466f93
ds-suyog <suyog.sethia@gmail.com>
2019-06-07 02:01:13 +0530

    decorator without args

--- decorators.py
+++ decorators.py
@@ -1,28 +1,17 @@
 
-# defining a decorator 
 def hello_decorator(func): 
-    # inner1 is a Wrapper function in which the argument is called  
-    # inner1 function can access the outer local functions like in this case "func" 
     def inner1():
         print("Hello, this is before myfunc execution") 
-  
-        # calling the actual function now inside the wrapper function. 
         func() 
-  
-        print("This is after myfunc execution") 
-          
+        print("This is after myfunc execution")      
     return inner1
-  
-  
-# defining a function, to be called inside wrapper 
+
 @hello_decorator
 def myfunc(): 
     print("This is inside the function !!") 
   
-# Either do this or write @hello_decorator above definition of 'myfunc'
 # passing 'myfunc' inside the decorator to control its behavior 
+# Either do this or write @hello_decorator above definition of 'myfunc'
 # myfunc = hello_decorator(myfunc) 
-  
-  
-# calling the function 
+
 myfunc() 
