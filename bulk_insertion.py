d71df872416c6a8801627d50e8b5272455923553
ds-suyog <suyog.sethia@gmail.com>
2019-06-07 21:39:33 +0530

    one by one insertion and bulk insertion

--- bulk_insertion.py
+++ bulk_insertion.py
@@ -2,26 +2,33 @@ import elasticsearch
 from elasticsearch import helpers, Elasticsearch
 import csv
 import pandas as pd 
+import git, sys; sys.path.append("{}/data".format(git.Repo('.', search_parent_directories=True).working_tree_dir))
 
-es = elasticsearch.Elasticsearch()
+es = Elasticsearch()
 
-file = r'/home/suyog/data/bank_details.xls'
+file = r'data/bank_details.xls'
 df = pd.read_excel(file)
-df.to_csv("output.csv", sep='\t', encoding='utf-8', index = False)
+df.to_csv("tmp/bank_details.csv", sep='\t', encoding='utf-8', index = False)
 
-reader = open('/home/suyog/github/IndianBanks/output.csv', 'r') 
+reader = open('tmp/bank_details.csv', 'r') 
 count = 0
 keys =  []
 values = []
+dict_list = list()
 for line in reader.readlines():
-  if count == 0:
-    keys = line.split('\t')
+    line = line.strip()
+    if count == 0:
+        keys = line.split('\t')
+    else:
+        values = line.split('\t')
+        dictt = zip(keys, values)
+        dictt = dict(dictt)
+        dictt_bulk = dict()
+        dictt_bulk.update({'_index': 'indian_banks_bulk','_type': 'detail','_id': count})
+        dict_list.append(dictt_bulk)
+        es.index(index='indian_banks', doc_type='detail', body= dictt)
     count += 1
-  else:
-    values = line.split('\t')
-    dictt = zip(keys, values)
-    dictt = dict(dictt)
-    es.index(index='indian_bank', doc_type='bank_test', body= dictt)
-    #helpers.bulk(es, dict(dictt), index='my-index2', doc_type='my-type2')
+    if count == 5:
+        break
 
-
+helpers.bulk(es, dict_list, chunk_size=1000, request_timeout=200)
